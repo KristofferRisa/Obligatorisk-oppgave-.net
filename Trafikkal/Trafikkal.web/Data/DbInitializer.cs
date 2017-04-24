c9ce0fe5973fac88ebeed6b905d5724b99bb0b8d
Kristoffer Risa <kristofferrisa@gmail.com>
2017-04-24 13:19:27 +0200

    Updated migrations and added questions in the DbInitializer

--- Trafikkal/Trafikkal.web/Data/DbInitializer.cs
+++ Trafikkal/Trafikkal.web/Data/DbInitializer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trafikkal.web.Models;
 
 namespace Trafikkal.web.Data
 {
@@ -12,22 +13,261 @@ namespace Trafikkal.web.Data
             context.Database.EnsureCreated();
 
             // Look for any students.
-            if (context.Quiz.Any())
+            if (context.Question.Any())
             {
                 return;   // DB has been seeded
             }
 
-            //var rooms = new Room[]
-            //{
-            //new Room{Floor = 1,RoomNumber =  101,RoomName = "101"},
-            //new Room{Floor = 3,RoomNumber =  314,RoomName = "314"},
-            //};
-            //foreach (var room in rooms)
-            //{
-            //    context.Rooms.Add(room);
-            //}
+            var questions = new List<Question>()
+            {
+                new Question()
+                {
+                    Number = 1,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 2,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 3,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 4,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 5,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 6,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 7,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 8,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 9,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 10,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 11,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 12,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 13,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 14,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 15,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 16,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 17,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 18,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 19,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                },
+                new Question()
+                {
+                    Number = 20,
+                    Text = "Hva er ",
+                    Alternative1 = "",
+                    Alternative2 = "",
+                    Alternative3 = "",
+                    Alternative4 = "",
+                    Answer = "4",
+                    Active = true,
+                    Created = DateTime.Now
+                }
+            };
+            
+            foreach (var question in questions)
+            {
+                context.Question.Add(question);
+            }
 
-            //context.SaveChanges();
+            context.SaveChanges();
         }
     }
 }
---
+++ Trafikkal/Trafikkal.web/Data/Migrations/20170424111347_quiz.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Trafikkal.web.Data.Migrations
+{
+    public partial class quiz : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Alternative = table.Column<string>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    QuestionNumber = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quiz",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MinScoreToPass = table.Column<decimal>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quiz", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    Alternative1 = table.Column<string>(nullable: true),
+                    Alternative2 = table.Column<string>(nullable: true),
+                    Alternative3 = table.Column<string>(nullable: true),
+                    Alternative4 = table.Column<string>(nullable: true),
+                    Answer = table.Column<string>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Img = table.Column<string>(nullable: true),
+                    Number = table.Column<int>(nullable: false),
+                    QuizId = table.Column<int>(nullable: true),
+                    Text = table.Column<string>(nullable: false),
+                    Video = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Question_Quiz_QuizId",
+                        column: x => x.QuizId,
+                        principalTable: "Quiz",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Question_QuizId",
+                table: "Question",
+                column: "QuizId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answers");
+
+            migrationBuilder.DropTable(
+                name: "Question");
+
+            migrationBuilder.DropTable(
+                name: "Quiz");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
